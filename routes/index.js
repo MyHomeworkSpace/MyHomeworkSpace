f08679023452bfb59d4d9c76cb87eb8d8b8fcdcd
thatoddmailbox <thatoddmailbox@users.noreply.github.com>
2015-10-27 11:35:02 -0400

    Another tester!

--- routes/index.js
+++ routes/index.js
@@ -39,7 +39,7 @@ router.post('/login', function(req, res, next) {
 		req.session.username = "zomg_testUser";
 		res.redirect(global.basePath + "/app");
 	}
-	if (!(req.body.username == "c19as3" || req.body.username == "c19ww" || req.body.username == "c19em" || req.body.username == "c19jf" || req.body.username == "c20wb" || req.body.username == "c21ys" || req.body.username == "c21as2")) {
+	if (!(req.body.username == "c19as3" || req.body.username == "c19ww" || req.body.username == "c19em" || req.body.username == "c19jf" || req.body.username == "c20wb" || req.body.username == "c21ys" || req.body.username == "c21as2" || req.body.username == "c21lw")) {
 		res.render('login', { title: 'Log in', error: 'You do not have permission to access this site.' });
 		return;
 	}
