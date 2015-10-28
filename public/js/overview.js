cacbc90c7bf221baf820311661befedd1636a4d9
em-p <gremleaf@gmail.com>
2015-10-28 13:58:42 -0400

    Update overview.js

--- public/js/overview.js
+++ public/js/overview.js
@@ -1,7 +1,44 @@
 window.overview = {};
-
+var score = 0;
+window.overview.getSubjects = function() {
+	//get the number of subjects here
+	window.overview.getHw();
+}
+window.overview.getHwPerSubjects = function() {
+	//get the number of hw for each subject
+	addScore = 0;
+	score = score + addScore
+}
+window.overview.getHwPerSubjects = funtion(score){
+	//display gif per score
+	if score = 0{
+		
+	}
+	if score = 0{
+		
+	}
+	if score = 0{
+		
+	}
+	if score = 0{
+		
+	}
+	if score = 0{
+		
+	}
+	if score = 0{
+		
+	}
+	if score = 0{
+		
+	}if score = 0{
+		
+	}
+}
 $(document).ready(function() {
 	$("#overview").on("tabOpened", function() {
 		$(".overview-date").text(window.utils.formatDate_english(new Date()));
 	});
+	window.overview.display();
+	
 });
