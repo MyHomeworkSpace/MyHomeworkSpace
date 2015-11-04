f7ac751fce5e269085b4a5184ddf02ce2450688f
thatoddmailbox <thatoddmailbox@users.noreply.github.com>
2015-11-04 14:33:05 -0500

    Some more my day stuff

---
+++ public/js/myDay.js
@@ -0,0 +1,12 @@
+window.myDay = {
+	
+};
+
+$(document).ready(function() {
+	$("#myDay").on("tabOpened", function() {
+		
+	});
+	$("#myDay-welcome-submit").click(function() {
+		alert("Doesn't work yet!");
+	})
+});
