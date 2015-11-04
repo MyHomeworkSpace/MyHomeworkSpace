8317a3e705f6ae2260494945e8ff3bf0c8921e21
thatoddmailbox <thatoddmailbox@users.noreply.github.com>
2015-11-04 14:49:59 -0500

    Add My Day API file

---
+++ routes/api_myDay.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+	res.json({
+		status: "ok",
+		version: "1"
+	});
+});
+
+module.exports = router;
