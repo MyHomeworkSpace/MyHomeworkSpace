1eee91e4e53e6954813fe115e69633a4b7b61ade
medensqui <student@c02kw1qnfh51.dalton.org>
2015-11-05 13:16:34 -0500

    fix clubs bug

--- public/js/myDay.js
+++ public/js/myDay.js
@@ -29,7 +29,7 @@ $(document).ready(function() {
 		prefs.sleepLength = $("input[name=sleepHrs]").val();
 		prefs.wakeUpTime = $("input[name=WakeUpTime]").val();
 		prefs.address = $("input[name=Address]").val();
-		prefs.clubs = $("input[name=clubs]").val();
+		prefs.clubs = $("select[name=clubs]").val();
 		prefs.school = $("input[name=school]").val();
 		window.api.post("myDay/updatePrefs", prefs, function(resp) {
 
