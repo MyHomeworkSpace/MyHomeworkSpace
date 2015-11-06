4d8fc05051c80b185f80629a76429305799f94e6
willweinstein <will@weinstein-nyc.com>
2015-11-05 22:26:11 -0500

    FIX ALL THE ERRORS

--- public/js/prefs.js
+++ public/js/prefs.js
@@ -5,14 +5,14 @@ window.prefs = {};
 $(document).ready(function() {
   window.api.get("prefs/get/name-subj", function(data) {
     var btnTrue = data.val;
-    if(btnTrue == 1) {
+    if(btnTrue == "1") {
       $("prefs-hwView-swap").prop("checked", true);
     }
-    $("#usr-btn").click(function() {
-      if(btnTrue == 1) {
-        btnTrue = 0;
+    $("#prefs-hwView-swap").change(function() {
+      if(btnTrue == "1") {
+        btnTrue = "0";
       } else {
-        btnTrue = 1;
+        btnTrue = "1";
       };
       window.api.post("prefs/set", { name: "name-subj", value: btnTrue}, function() {});
     });
