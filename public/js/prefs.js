868d88d8e4c49cfc5ae110dfa15538b3a3109d55
thatoddmailbox <thatoddmailbox@users.noreply.github.com>
2015-11-06 11:23:23 -0500

    Begin prefs modal

--- public/js/prefs.js
+++ public/js/prefs.js
@@ -1,28 +1,31 @@
 window.prefs = {};
 
-
+window.prefs.openModal = function(feature) {
+	$("#prefs-feature-name").text(window.utils.getLongNameForFeature(feature));
+	$("#prefs-modal").modal();
+};
 
 $(document).ready(function() {
-  window.api.get("prefs/get/name-subj", function(data) {
-    var btnTrue = data.val;
-    if(btnTrue == "1") {
-      $("#prefs-hwView-swap").prop("checked", true);
-    }
-    $("#prefs-hwView-swap").change(function() {
-      if(btnTrue == "1") {
-        btnTrue = "0";
-      } else {
-        btnTrue = "1";
-      };
-      window.api.post("prefs/set", { name: "name-subj", value: btnTrue}, function() {});
-    });
-    
-  });
-  $("#usr-btn").click(function() {
-    var usrname = $("#usr-name").val();
-    window.api.post("prefs/setName", { name: usrname }, function() {
-      window.page.showLoading();
-      window.location.reload();
-    });
-  });
+	window.api.get("prefs/get/name-subj", function(data) {
+		var btnTrue = data.val;
+		if(btnTrue == "1") {
+			$("#prefs-hwView-swap").prop("checked", true);
+		}
+		$("#prefs-hwView-swap").change(function() {
+			if(btnTrue == "1") {
+				btnTrue = "0";
+			} else {
+				btnTrue = "1";
+			};
+			window.api.post("prefs/set", { name: "name-subj", value: btnTrue}, function() {});
+		});
+		
+	});
+	$("#usr-btn").click(function() {
+		var usrname = $("#usr-name").val();
+		window.api.post("prefs/setName", { name: usrname }, function() {
+			window.page.showLoading();
+			window.location.reload();
+		});
+	});
 });
