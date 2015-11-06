f4ab23e915ee44a136a2677d823d5798890136a3
willweinstein <will@weinstein-nyc.com>
2015-11-05 22:23:42 -0500

    Update prefs.js

--- public/js/prefs.js
+++ public/js/prefs.js
@@ -5,16 +5,16 @@ window.prefs = {};
 $(document).ready(function() {
   window.api.get("prefs/get/name-subj", function(data) {
     var btnTrue = data.val;
-  });
-  if(btnTrue == 1) {
-    $("prefs-hwView-swap).prop("checked", true);
-  }
-  $("#usr-btn").click(function() {
     if(btnTrue == 1) {
-      btnTrue = 0;
-    } else {
-      btnTrue = 1;
-    };
-    window.api.post("prefs/set", { name: "name-subj", value: btnTrue}, function() {});
+      $("prefs-hwView-swap").prop("checked", true);
+    }
+    $("#usr-btn").click(function() {
+      if(btnTrue == 1) {
+        btnTrue = 0;
+      } else {
+        btnTrue = 1;
+      };
+      window.api.post("prefs/set", { name: "name-subj", value: btnTrue}, function() {});
+    });
   });
 });
