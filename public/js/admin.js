5a595da77b9a94616ef495cae8712394d63152d3
thatoddmailbox <thatoddmailbox@users.noreply.github.com>
2015-11-06 22:49:09 -0500

    Update admin.js

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,5 +1,15 @@
 window.admin = {};
 
 $(document).ready(function() {
-	
+	$("#admin-feedback").on("tabOpened", function() {
+		window.api.get("admin/feedback/getList", function(resp) {
+			$("#admin-feedback-list").text("");
+			for (var feedbackIndex in resp.feedback) {
+				var feedbackItem = resp.feedback[feedbackIndex];
+				var $feedbackLi = $('<li></li>');
+					$feedbackLi.text(feedbackItem.name);
+				$("#admin-feedback-list").append($feedbackLi);
+			};
+		});
+	});
 });
