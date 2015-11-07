0421622308ec076df4ac7c3736a7ed6aff259e28
thatoddmailbox <thatoddmailbox@users.noreply.github.com>
2015-11-06 22:32:14 -0500

    Update api_admin.js

--- routes/api_admin.js
+++ routes/api_admin.js
@@ -8,6 +8,20 @@ router.get('/', function(req, res, next) {
 	});
 });
 
+router.get('/feedback/getList', global.apiCall, global.requireUser, global.getUserRecord, global.requireViewFeedback, function(req, res, next) {
+	knex("feedback").select("feedbackId, msg, name").then(function(obj) {
+		res.json({
+			status: "ok",
+			feedback: obj
+		});	
+	}).catch(function() {
+		res.json({
+			status: "error",
+			error: "Unknown database error."
+		});	
+	});
+});
+
 router.get('/feedback/get/:id', global.apiCall, global.requireUser, global.getUserRecord, global.requireViewFeedback, function(req, res, next) {
 	res.json({
 		status: "ok",
