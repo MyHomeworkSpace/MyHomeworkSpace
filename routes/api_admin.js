5e7cbee3d7e8836aeb05a4141d99c8b8e80ea83c
thatoddmailbox <thatoddmailbox@users.noreply.github.com>
2015-11-06 22:36:58 -0500

    Update api_admin.js

--- routes/api_admin.js
+++ routes/api_admin.js
@@ -9,24 +9,32 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/feedback/getList', global.apiCall, global.requireUser, global.getUserRecord, global.requireViewFeedback, function(req, res, next) {
-	knex("feedback").select("feedbackId, msg, name").then(function(obj) {
+	knex("feedback").select("feedbackId", "msg", "name").then(function(obj) {
 		res.json({
 			status: "ok",
 			feedback: obj
 		});	
-	}).catch(function(e) {
+	}).catch(function() {
 		res.json({
 			status: "error",
-			error: "Unknown database error.",
-			stack: e
+			error: "Unknown database error."
 		});	
 	});
 });
 
 router.get('/feedback/get/:id', global.apiCall, global.requireUser, global.getUserRecord, global.requireViewFeedback, function(req, res, next) {
-	res.json({
-		status: "ok",
-		version: "1"
+	knex("feedback").where({
+		feedbackId: req.params.id
+	}).select("*").then(function(obj) {
+		res.json({
+			status: "ok",
+			feedback: obj[0]
+		});
+	}).catch(function() {
+		res.json({
+			status: "error",
+			error: "Unknown database error."
+		});	
 	});
 });
 
