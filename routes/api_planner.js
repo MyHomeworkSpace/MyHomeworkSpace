e0fdc926f1d31bf3b7953f41a904478e4b24af80
thatoddmailbox <thatoddmailbox@users.noreply.github.com>
2015-11-08 11:04:58 -0500

    WORK PLZdsafasdf

--- routes/api_planner.js
+++ routes/api_planner.js
@@ -329,7 +329,10 @@ router.post('/sections/swap', global.apiCall, global.requireUser, global.getUser
 		return;
 	}
 	// verify both sections exist
-	knex("planner_sections").select("*").where("sectionIndex", req.params.first).orWhere("sectionIndex", req.params.second).then(function(obj) {
+	knex("planner_sections").select("*").where("userId", req.locals.user.id).
+		where(function() {
+			this.where("sectionIndex", req.params.first).orWhere("sectionIndex", req.params.second)
+		}).then(function(obj) {
 		if (obj.length != 2) {
 			res.json({
 				status: "error",
