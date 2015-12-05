86e55586d3ed719d81aac7385af1c991fcda2f96
willweinstein <will@weinstein-nyc.com>
2015-12-05 12:20:45 -0500

    hopefully does something. someone pls look quickly
    hexing

--- public/js/planner.js
+++ public/js/planner.js
@@ -123,11 +123,20 @@ window.planner.createSubjectRow = function(subjectName, subjectIndex) {
 
 				$editCell.children("textarea").width("150px");
 				$editCell.children("textarea").height("100px");
-				$editCell.children("textarea").highlightTextarea({
-					words: window.utils.getPrefixes(),
+				for (child in $editCell.children("textarea")) {
+					var realWords = window.utils.getPrefixes()
+					realWords.push({color:"suprTitle", words:[$(this).parent().parent().parent().attr("data-subjectName")]})
+					$(this).highlightTextArea({
+						words: realWords,
+						firstWord: true,
+						caseSensitive: false
+					});
+				}
+				/*$editCell.children("textarea").highlightTextarea({
+					words: realWords,
 					firstWord: true,
 					caseSensitive: false 
-				});
+				}); */
 				
 				$editCell.find("textarea").keydown(function (evt) {
 					var keycode = evt.charCode || evt.keyCode;
