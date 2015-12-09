e9d41414f7998c43e7367873395dfbeb849ed931
willweinstein <will@weinstein-nyc.com>
2015-12-08 22:15:09 -0500

    i am knexr
    all your base are belong to me

--- routes/api_myDay.js
+++ routes/api_myDay.js
@@ -7,7 +7,61 @@ router.get('/', function(req, res, next) {
 		version: "1"
 	});
 });
-
+router.post('/info/set', global.apiCall, global.requireUser, global.getUserRecord, function(req, res, next) {
+	if(req.body.sleep == undefined || parseInt(req.body.sleep) == NaN || parseInt(req.body.sleep) <= 0) {
+		res.json({
+			status: "error",
+			error: "Missing or invalid sleep parameter!"
+		});
+		return;
+	}
+	if(req.body.wake == undefined || moment(req.body.wake).format('h:mm:ss a') == "Invalid date") {
+		res.json({
+			status: "error",
+			error: "Missing or invalid wake parameter!"
+		});
+		return;
+	}
+	if(req.body.address == undefined) {
+		res.json({
+			status: "error",
+			error: "Missing address parameter!"
+		});
+		return;
+	}
+	if(req.body.schoolId == undefined || parseInt(req.body.schoolId) == NaN) {
+		res.json({
+			status: "error",
+			error: "Missing or invalid schoolId parameter!"
+		});
+		return;
+	}
+	knex("myDay").select("*").where({
+		userId: res.locals.user.id
+	}).then(function() {
+		knex("myDay").insert({
+			sleep: parseInt(req.body.sleep),
+			wake: req.body.wake,
+			address: req.body.address,
+			clubs: req.body.clubs,
+			schoolId: parseInt(req.body.schoolId)
+		}).then(function() {
+			res.json({
+				status: "ok",
+			});
+		}).catch(function() {
+			res.json({
+				status: "error",
+				error: "Unknown database error."
+			});
+		});
+	}).catch(function() {
+		res.json({
+			status: "error",
+			error: "Unknown database error."
+		});
+	});
+}
 router.get('/clubs/getAll/', function(req, res, next) {
 	knex("clubs").select("*").then(function(data) {
 		res.json({
