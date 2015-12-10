ace2b5a0088b7944e3eec24d3b133e5e808b5948
willweinstein <will@weinstein-nyc.com>
2015-12-09 19:20:45 -0500

    Update myDay.js

--- public/js/myDay.js
+++ public/js/myDay.js
@@ -17,9 +17,6 @@ window.myDay.loadClubs = function(target, callback) {
 };
 
 $(document).ready(function() {
-	window.myDay.loadClubs($(".myDay-club-select"), function() {
-		$(".myDay-club-select").chosen();
-	});
 	$("#myDay").on("tabOpened", function() {
 		
 	});
