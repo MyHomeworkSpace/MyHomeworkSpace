5580562f0c00a3890e1639e76cacfaf729569fbe
willweinstein <will@weinstein-nyc.com>
2015-12-09 19:31:20 -0500

    oops
    all your base are belong to us

--- public/js/myDay.js
+++ public/js/myDay.js
@@ -17,6 +17,9 @@ window.myDay.loadClubs = function(target, callback) {
 };
 
 $(document).ready(function() {
+	window.myDay.loadClubs($(".myDay-club-select"), function() {
+-		$(".myDay-club-select").chosen();
+-	});
 	$("#myDay").on("tabOpened", function() {
 		
 	});
