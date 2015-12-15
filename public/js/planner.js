e3557db5ed28bc64d321174b0ae69ad89d6b1c5a
willweinstein <will@weinstein-nyc.com>
2015-12-15 16:28:31 -0500

    input system == fussy
    all your base are belong to us

--- public/js/planner.js
+++ public/js/planner.js
@@ -200,6 +200,7 @@ window.planner.createSubjectRow = function(subjectName, subjectIndex) {
 				$editCell.find("textarea").click(function(clicky) {
 					if(clicky.altKey) {
 						$(this).val("none");
+						$(this).trigger("input");
 					};
 				});
 				var $mic = $('<div class="magic-input-container"></div>');
