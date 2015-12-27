c606a65ba3d2d0da87cfed0ab26d41a1ce8b26fe
thatoddmailbox <thatoddmailbox@users.noreply.github.com>
2015-12-27 11:30:02 -0500

    Return proper status codes

--- app.js
+++ app.js
@@ -238,7 +238,7 @@ app.use(function(req, res, next) {
 // will print stacktrace
 if (app.get('env') === 'development') {
 	app.use(function(err, req, res, next) {
-		//res.status(err.status || 500);
+		res.status(err.status || 500);
 		res.render('error', {
 			message: err.message,
 			error: err
