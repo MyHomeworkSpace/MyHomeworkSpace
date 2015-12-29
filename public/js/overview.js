47cf3de80e5e3348f27f93631794ebc639043488
willweinstein <will@weinstein-nyc.com>
2015-12-29 09:42:45 -0700

    hopefully loads announcements
    all your base are belong to us

--- public/js/overview.js
+++ public/js/overview.js
@@ -4,4 +4,17 @@ $(document).ready(function() {
 	$("#overview").on("tabOpened", function() {
 		$(".overview-date").text(window.utils.formatDate_english(new Date()));
 	});
+	window.api.get('overview/announcements/get/', function(data) {
+		var response = data.feedback;
+		for(feed in response) {
+			if(moment(feed.time).add(feed.days, "days").isBefore(moment())) {
+				var $feedLi = $("<li></li>");
+					$feedLi.text(feed.msg);
+					$("#announcementList").append($feedLi);
+			}
+		}
+		if($("#announcementList").children().size() > 1) {
+			$("#noAnnouncements").remove()
+		}
+	});
 });
