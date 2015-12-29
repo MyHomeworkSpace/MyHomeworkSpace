6c837a8c7f61c504bdd07108ce683e4c08423105
willweinstein <will@weinstein-nyc.com>
2015-12-29 11:18:07 -0700

    oops
    all your base are belong to us

--- public/js/admin.js
+++ public/js/admin.js
@@ -45,7 +45,7 @@ $(document).ready(function() {
 					var $removeBtn = $('<button class="btn btn-xs btn-danger"><i class="fa fa-trash-o"></i></button>');
 						$removeBtn.click(function() {
 							var idThingy = $(this).parent().attr("data-id");
-							window.api.post('admin/announcements/remove', {announcementId: idThingy}, function(result){});
+							window.api.post('admin/announcements/remove', {id: idThingy}, function(result){});
 						});
 					$feedLi.append($removeBtn)
 				$("#announcement-delete-list").append($feedLi);	
