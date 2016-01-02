bfa3026e983179ab81008a3d8a4ec978f3ae83d4
thatoddmailbox <thatoddmailbox@users.noreply.github.com>
2016-01-01 22:31:15 -0500

    fix flickering when there is no announcement

--- public/js/overview.js
+++ public/js/overview.js
@@ -3,18 +3,18 @@ window.overview = {};
 $(document).ready(function() {
 	$("#overview").on("tabOpened", function() {
 		$(".overview-date").text(window.utils.formatDate_english(new Date()));
-	});
-	window.api.get('overview/announcements/get/', function(data) {
-		var response = data.feedback;
-		for(feed in response) {
-			if(moment(response[feed].time).add(response[feed].days, "days").isAfter(moment())) {
-				var $feedLi = $("<li></li>");
-					$feedLi.append(response[feed].msg);
+		window.api.get('overview/announcements/get/', function(data) {
+			var response = data.feedback;
+			for (var feed in response) {
+				if (moment(response[feed].time).add(response[feed].days, "days").isAfter(moment())) {
+					var $feedLi = $("<li></li>");
+						$feedLi.append(response[feed].msg);
 					$("#announcementList").append($feedLi);
+				}
 			}
-		}
-		if($("#announcementList").children().size() < 1) {
-			$(".alert-overview").remove()
-		}
+			if ($("#announcementList").children().size() > 0) {
+				$("#announcementAlert").css("display", "block");
+			}
+		});
 	});
 });
