8d56d1f50d26053d18b1cbd11d19c422c91b273a
RobotXLabs <databasemaster9233@gmail.com>
2016-01-02 20:13:57 -0500

    more stuff for #9

--- public/js/labs.js
+++ public/js/labs.js
@@ -1,6 +1,9 @@
+$("#schedules-connect").click(function() {
+  $("#login-to-schedules-modal").modal();
+});
 $("#schedule-lab-student").click(function() {
-  swal("Schedule A Lab Page Goes Here...", "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", "warning");
-}
+  swal("Schedule a Lab!", "Schedule a Lab with a teacher. Just click the button below...", "success");
+});
 $("#accept-labs-teacher").click(function() {
-  swal("Nice!", "You are now accepting Labs from students! (Pssst guys make this do something)", "warning");
-}
+  swal("Nice!", "You are now accepting Labs from students! (Pssst guys make this do something)", "success");
+});
