25400c9a0bfa804367cab1a269e92aed9e48718f
RobotXLabs <databasemaster9233@gmail.com>
2016-01-02 19:15:05 -0500

    Sweetalerts

---
+++ public/js/labs.js
@@ -0,0 +1,6 @@
+$("#schedule-lab-student").click(function() {
+  swal("Schedule A Lab Page Goes Here...", "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", "warning");
+}
+$("#accept-labs-teacher").click(function() {
+  swal("Nice!", "You are now accepting Labs from students! (Pssst guys make this do something)", "warning");
+}
