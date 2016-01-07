9a87d701b225e4281afd9d3f53ca24b82f6d801b
willweinstein <will@weinstein-nyc.com>
2016-01-06 21:09:20 -0500

    yay
    all your base are belong to us

--- routes/api_admin.js
+++ routes/api_admin.js
@@ -104,4 +104,18 @@ router.post('/announcements/remove/', global.apiCall, global.requireUser, global
 	});
 });
 
+router.get('/users/count/', global.apiCall, global.requireUser, global.getuserRecord, global.requireNonZeroLevel, function(req, res, next) {
+	knex("users").select("id").then(function(res) {
+		res.json({
+			status: "ok",
+			number: res.length
+		});
+	}).catch(function() {
+		res.json({
+			status: "error",
+			error: "Unknown database error"
+		});
+	})''
+});
+
 module.exports = router;
