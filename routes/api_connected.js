a965035573f368e4cd7e1ad4fef1f42a6ea2e33e
thatoddmailbox <thatoddmailbox@users.noreply.github.com>
2016-02-06 18:37:21 -0500

    wheye you no work

--- routes/api_connected.js
+++ routes/api_connected.js
@@ -83,6 +83,7 @@ router.post('/schedules/connect', global.apiCall, global.requireUser, global.get
 						parseXmlString(chunk, function(err, data) {
 							res.json({
 								status: "ok",
+								chunk: chunk,
 								data: data
 							});
 						});
