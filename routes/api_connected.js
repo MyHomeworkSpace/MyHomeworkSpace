0cdcfcd5e96b05aed456d8867f833d390b742589
thatoddmailbox <thatoddmailbox@users.noreply.github.com>
2016-02-19 20:16:05 -0500

    sanity checking

--- routes/api_connected.js
+++ routes/api_connected.js
@@ -24,62 +24,73 @@ router.post('/schedules/connect', global.apiCall, global.requireUser, global.get
 		return;
 	}
 
-	var username = req.body.username;
-	var password = req.body.password;
-
-	var dataStr = "";
-	dataStr += "<credentials><username>";
-	dataStr += username.encodeHTML();
-	dataStr += "</username><password type=\"plaintext\">";
-	dataStr += password.encodeHTML();
-	dataStr += "</password></credentials>";
-
-	roux.request("", "authenticate", dataStr, function(err) {
-		res.json({
-			status: "error",
-			message: err
-		});
-	}, function(data) {
-		var key = data["response"]["result"][0]["key"][0]["_"];
-		var owner = data["response"]["result"][0]["key"][0]["$"]["owner"];
-
-		roux.request(key, "selectObjects", "<object><objecttype><name>academicyear</name></objecttype></object><pattern><sortorder><academicyear><open>desc</open></academicyear></sortorder><limit>1</limit></pattern>", function(msg, err) {
+	knex.select("*").from("schedule").where({
+		userId: res.locals.user.id
+	}).then(function(data) {
+		if (data.length == 0) {
 			res.json({
 				status: "error",
-				message: msg,
-				err: err
+				error: "You've already imported your schedule!"
+			});
+			return;
+		}
+		var username = req.body.username;
+		var password = req.body.password;
+
+		var dataStr = "";
+		dataStr += "<credentials><username>";
+		dataStr += username.encodeHTML();
+		dataStr += "</username><password type=\"plaintext\">";
+		dataStr += password.encodeHTML();
+		dataStr += "</password></credentials>";
+
+		roux.request("", "authenticate", dataStr, function(err) {
+			res.json({
+				status: "error",
+				message: err
 			});
 		}, function(data) {
-			var academicyear = data["response"]["result"][0]["academicyear"][0]["$"]["SSID"];
+			var key = data["response"]["result"][0]["key"][0]["_"];
+			var owner = data["response"]["result"][0]["key"][0]["$"]["owner"];
 
-			var start = utils.findNextMonday();
-			var end = new Date(start);
-			end.setDate(end.getDate() + 4);
-
-			dataStr = "";
-			dataStr += "<request><key>";
-			dataStr += key.encodeHTML();
-			dataStr += "</key><action>selectStudentCalendar</action><ID>";
-			dataStr += owner.encodeHTML();
-			dataStr += "</ID><academicyear>";
-			dataStr += academicyear;
-			dataStr += "</academicyear><start>";
-			dataStr += utils.formatDate_roux(start);
-			dataStr += "</start><end>";
-			dataStr += utils.formatDate_roux(end);
-			dataStr += "</end></request>";
-
-			roux.request(key, "selectStudentCalendar", dataStr, function(msg, err) {
+			roux.request(key, "selectObjects", "<object><objecttype><name>academicyear</name></objecttype></object><pattern><sortorder><academicyear><open>desc</open></academicyear></sortorder><limit>1</limit></pattern>", function(msg, err) {
 				res.json({
 					status: "error",
 					message: msg,
 					err: err
 				});
-				return;
-			}, function(data, chunk) {
-				res.json({
-					status: "ok",
-					data: data
+			}, function(data) {
+				var academicyear = data["response"]["result"][0]["academicyear"][0]["$"]["SSID"];
+
+				var start = utils.findNextMonday();
+				var end = new Date(start);
+				end.setDate(end.getDate() + 4);
+
+				dataStr = "";
+				dataStr += "<request><key>";
+				dataStr += key.encodeHTML();
+				dataStr += "</key><action>selectStudentCalendar</action><ID>";
+				dataStr += owner.encodeHTML();
+				dataStr += "</ID><academicyear>";
+				dataStr += academicyear;
+				dataStr += "</academicyear><start>";
+				dataStr += utils.formatDate_roux(start);
+				dataStr += "</start><end>";
+				dataStr += utils.formatDate_roux(end);
+				dataStr += "</end></request>";
+
+				roux.request(key, "selectStudentCalendar", dataStr, function(msg, err) {
+					res.json({
+						status: "error",
+						message: msg,
+						err: err
+					});
+					return;
+				}, function(data, chunk) {
+					res.json({
+						status: "ok",
+						data: data
+					});
 				});
 			});
 		});
