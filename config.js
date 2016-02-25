2ad8061f3f5032600b72eeec6f02be45516d9c1c
thatoddmailbox <thatoddmailbox@users.noreply.github.com>
2016-02-24 21:54:17 -0500

    hopefully this doesn't mess everything up... plox plox ploox

--- config.js
+++ config.js
@@ -4,4 +4,17 @@ module.exports = {
 		user     : 'planhub',
 		password : 'planhub',
 		database : 'planhub'}
+	emails: {
+		enabled: false,
+		from: "Misconfigured PlanHub <misconfigured@misconfigured.invalid>",
+		smtpConfig: {
+			host: "localhost",
+			port: 465,
+			secure: true
+			auth: {
+				user: "accounts@planhub.me",
+				pass: "password"
+			}
+		}
+	}
 };
