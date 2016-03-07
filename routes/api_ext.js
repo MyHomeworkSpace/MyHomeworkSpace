a9d0fc202a136ea9462523eb3477f41cb215f854
thatoddmailbox <thatoddmailbox@users.noreply.github.com>
2016-03-07 08:55:55 -0500

    name name name name naaaame

--- routes/api_ext.js
+++ routes/api_ext.js
@@ -16,7 +16,7 @@ router.get('/getAuthPage', global.apiCall, function(req, res, next) {
 });
 
 router.get("/appAuth/:token", function(req, res, next) {
-	res.render("externalapp", { title: 'PlanHub', appName: "DaltonTab" });
+	res.render("externalapp", { title: 'MyHomeworkSpace', appName: "DaltonTab" });
 });
 
 module.exports = router;
