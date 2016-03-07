62dc8540da4d529e4d58d6a7e1fdf6768510b619
thatoddmailbox <thatoddmailbox@users.noreply.github.com>
2016-03-07 08:54:23 -0500

    name changing again again again

--- emails.js
+++ emails.js
@@ -1,8 +1,8 @@
 var emailTemplates = {
 	verify: {
 		subject: "Verify your email",
-		html: '<h1>PlanHub</h1><h2>Verify your email</h2><p>TODO: some text here I guess</p><a href="(verify_url)">Click here to verify your email</a><p>If that didn\'t work, try copy-pasting the following URL into a new browser window: (verify_url)</p>',
-		text: 'PlanHub\nVerify your email\nTODO: some text here I guess\nCopy-paste the following URL into a new browser window: (verify_url)'
+		html: '<h1>MyHomeworkSpace</h1><h2>Verify your email</h2><p>TODO: some text here I guess</p><a href="(verify_url)">Click here to verify your email</a><p>If that didn\'t work, try copy-pasting the following URL into a new browser window: (verify_url)</p>',
+		text: 'MyHomeworkSpace\nVerify your email\nTODO: some text here I guess\nCopy-paste the following URL into a new browser window: (verify_url)'
 	}
 };
 
