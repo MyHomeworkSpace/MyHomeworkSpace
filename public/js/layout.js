8f792c00a9bf58c0110a94c2dcda2423e4d227fe
nywillb <nywillb@users.noreply.github.com>
2016-03-11 13:33:12 -0500

    Create layout.js

---
+++ public/js/layout.js
@@ -0,0 +1,3 @@
+if (ref.match(/^https?:\/\/([^\/]+\.)?planhub\.me(\/|$)/i)) {
+  swal("We changed names!", "Now we you can get to you planner by going to myhomework.space, not planhub.me", "warning")
+}
