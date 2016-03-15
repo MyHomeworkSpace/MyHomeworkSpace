f09dd59f6153085e73ae8d8358a713d059ab821a
thatoddmailbox <thatoddmailbox@users.noreply.github.com>
2016-03-15 13:38:16 -0400

    this will be handled server-side and show the domainchange page, use that repo

--- public/js/layout.js
+++
@@ -1,3 +0,0 @@
-if (ref.match(/^https?:\/\/([^\/]+\.)?planhub\.me(\/|$)/i)) {
-  swal("We changed names!", "Now we you can get to you planner by going to myhomework.space, not planhub.me", "warning")
-}
