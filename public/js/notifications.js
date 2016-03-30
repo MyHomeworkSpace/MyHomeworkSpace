965b6e10875bcb7738091b03bb61da22e210e17d
nywillb <william@barkoffusa.com>
2016-03-30 14:43:59 +0000

    add icon to notification

--- public/js/notifications.js
+++ public/js/notifications.js
@@ -1,5 +1,6 @@
 var testNotification = new Notify('Yay!', {
     body: 'If you are seeing this, then notifications have been fully set up and are functional!',
+    icon: window.page.getStaticUrl() + "images/notificationicon.svg" ,
     notifyShow: testNotify
 });
 
@@ -26,7 +27,8 @@ function onNotifyPermissionDenied() {
     console.warn('Notification permission denied. :(');
     swal({
         title: 'Oops',
-        text: 'You denied access for us to send you desktop notifications. Here are instructions for how to accept access for them: <a href="https://support.google.com/chrome/answer/6148059?hl=en&ref_topic=3434353">Chrome</a>, <a href="https://support.mozilla.org/en-US/kb/push-notifications-firefox">Firefox</a>, and <a href="https://support.apple.com/kb/PH21493?locale=en_US&viewlocale=en_US">Safari</a>. If you need more help, you can contact us at <a href="mailto:hello@myhomework.space">hello@myhomework.space</a>.',
+        text: 'You denied access for us to send you desktop notifications. Here are instructions for how to accept access for them: <a href="https://support.google.com/chrome/answer/6148059?hl=en&ref_topic=3434353">Chrome</a>, <a href="https://support.mozilla.org/en-US/kb/push-notifications-firefox">Firefox</a>, and <a href="https://support.apple.com/kb/PH21493?locale=en_US&viewlocale=en_US">Safari</a>. If you need more help, you can contact us at <a href="mailto:hello@myhomework.space">hello@myhomework.space</a>. You can always disable desktop notifications in settings.',
+        type: "warning",
         html: true
     });
 }
