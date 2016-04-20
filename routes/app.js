54fd64311ca63eccd89f066309bff19c298fac6f
thatoddmailbox <asreg123@gmail.com>
2016-04-20 11:51:43 -0400

    should maybe fix carriers stuff

--- routes/app.js
+++ routes/app.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* App home page. */
 router.get('/', global.requireUser, global.getUserRecord, function(req, res, next) {
-	res.render('app', { title: 'MyHomeworkSpace' });
+	res.render('app', { title: 'MyHomeworkSpace', carriers: require("../sms") });
 });
 
 module.exports = router;
