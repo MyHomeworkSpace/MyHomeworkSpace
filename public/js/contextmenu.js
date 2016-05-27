d05e6a4044b10e125029fefec455b1fe403c45fa
nywillb <nywillb@users.noreply.github.com>
2016-05-27 17:39:09 -0400

    Update contextmenu.js

--- public/js/contextmenu.js
+++ public/js/contextmenu.js
@@ -1,3 +1,11 @@
+context.init({
+    fadeSpeed: 100,
+    filter: function ($obj){},
+    above: 'auto',
+    preventDoubleContext: true,
+    compress: false  
+})
+
 function cutcopypaste() {
     swal("Cut, Copy, and Paste in MyHomeworkSpace", "Due to some browser constraints, you must use keyboard shortcuts to cut, copy, and paste in MyHomeworkSpace. To cut, highlight the text you want to cut, and press Control/Command + X. To copy, higlight the text you want to copy, and press Control/Command + C. To paste, place your cursor where you would like to paste, and press Control/Command + V. Thank you!", "warning");
 }
